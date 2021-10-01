dae3d7e4bc2e1d2538c19e818018c5caed87769e
Mayur2905 <mayurgaikwad0101@gmail.com>
2021-10-01 14:40:45 +0530

    Display History

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React ,{useState} from "react";
 import Board from './components/Board';
 import { calculateWinner } from "./helpers";
+import History  from './components/History';
 
 import "./style/root.scss";
 
@@ -37,12 +38,16 @@ const App = () => {
     setCurrentMove(prev=>prev+1);
    
   };
+  const moveTo =(move)=>{
+    setCurrentMove(move);
+  }
 return(
   <div className="app" >
     <h1>TIC TAC TOE</h1>
     <h2>{message}</h2>
     <Board board={current.board} handleSquareClick={handleSquareClick}/>
+    <History history ={history} moveTo={moveTo} currentMove={currentMove}/>
   </div>
 );
-}
+};
 export default App;
