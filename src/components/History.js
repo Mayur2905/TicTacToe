c4b3ba66d2b8dc0fee9439f76d80d954071c9413
Mayur2905 <mayurgaikwad0101@gmail.com>
2021-10-02 21:59:00 +0530

    appied dynamics style

--- src/components/History.js
+++ src/components/History.js
@@ -2,14 +2,13 @@ import React from 'react'
 
 export const History = ({history,moveTo,currentMove}) => {
     return (
-        <ul>
+        <div className="history-wrapper" >
+        <ul className="history">
             {
                 history.map((_,move) => {
                     return <li key={move}>
                         <button 
-                        style={{
-                            fontWeight: move === currentMove ? 'bold' : 'normal',
-                        }}
+                        className={`btn-move ${move === currentMove ?'avtive':''}`}
                         type="button"
                          onClick={()=>{
                             moveTo(move);             
@@ -20,6 +19,7 @@ export const History = ({history,moveTo,currentMove}) => {
             }
             
         </ul>
+        </div>
     );
 };
 export default History;
--- src/components/StatusMessages.js
+++ src/components/StatusMessages.js
@@ -1,16 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-export const StatusMessages = ({winner,current}) => {
-        // const message = winner 
-        // ? `Winner is ${winner}`
-        // :`Next player is ${current.isXNext ? 'X' : 'O'}`;
-        const noMovesLeft = current.board.every(el=> el !== null);
-        return(
-        <h2>
-           {winner && `Winner is ${winner}`}
-           {!winner && !noMovesLeft &&`Next player is ${current.isXNext ? 'X' : 'O'}`}
-            {!winner && noMovesLeft && 'X and O Tied!!'}
-        </h2>
-        )
+const StatusMessage = ({ winner, current }) => {
+  const noMovesLeft = current.board.every(el => el !== null);
+
+  return (
+    <div className="status-message">
+      {winner && (
+      <>
+      winnner is{' '}
+      <span className={winner ==='X'?'text-green':'text-orange'}>
+              {winner}
+      </span>
+      </>
+      )}
+      {!winner &&
+        !noMovesLeft &&(
+        <>
+        Next player is <span className = {current.isXNext ? 'text-green' : 'text-orange'}>
+          {current.isXNext ? 'X':'O'}{' '}
+        </span>
+        </>
+        )}
+      {!winner && noMovesLeft && <>
+      <span className="text-green"> X </span> and <span className="text-orange"> O </span>Tied!!
+      </>}
+    </div>
+  );
 };
-export default StatusMessages;
+
+export default StatusMessage;
